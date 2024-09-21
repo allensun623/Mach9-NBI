5a8c3582728ca7cd766cf601be34d1d86204c97a
Allen Sun <allensun623@gmail.com>
2024-09-21 10:17:15 -0700

    remove scene and buildingTileset

--- client/src/components/Map/CesiumMap.jsx
+++ client/src/components/Map/CesiumMap.jsx
@@ -1,10 +1,5 @@
-import {
-  Cartesian3,
-  Math as CesiumMath,
-  createOsmBuildingsAsync,
-} from 'cesium';
-import { useEffect, useRef, useState } from 'react';
-import { CameraFlyTo, Scene, Viewer } from 'resium';
+import { Cartesian3, Math as CesiumMath } from 'cesium';
+import { CameraFlyTo, Viewer } from 'resium';
 // import StateMap from './StateMap';
 import { theme } from 'antd';
 import BridgeEntities from './BridgeEntities';
@@ -13,22 +8,6 @@ export default function CesiumMap() {
   const LAT = 40.003323;
   const LON = -77.194527;
 
-  // const cesium = useRef(null);
-  const sceneRef = useRef(null);
-  const [buildingTileset, setBuildingTileset] = useState();
-
-  useEffect(() => {
-    const addBuildingTileset = async () => {
-      const tileset = await createOsmBuildingsAsync();
-      setBuildingTileset(tileset);
-    };
-
-    addBuildingTileset();
-  }, []);
-
-  useEffect(() => {
-    // sceneRef.current?.cesiumElement?.primitives.add(buildingTileset);
-  }, [buildingTileset]);
   const { token } = theme.useToken();
   const mapStyle = {
     marginBottom: 4,
@@ -39,8 +18,8 @@ export default function CesiumMap() {
     width: '100%',
     padding: token.paddingSM,
   };
+
   return (
-    // <Viewer full>
     <Viewer style={mapStyle}>
       <CameraFlyTo
         destination={Cartesian3.fromDegrees(LON, LAT, 500_000)}
@@ -49,7 +28,6 @@ export default function CesiumMap() {
           pitch: CesiumMath.toRadians(-90),
         }}
       />
-      <Scene ref={sceneRef} />
       <BridgeEntities />
     </Viewer>
   );
