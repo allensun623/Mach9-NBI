9e26bbb129c93a20aeacf55f9d9a244e3def6201
Allen Sun <allensun623@gmail.com>
2024-09-22 20:41:06 -0700

    start animation of cameraFlyTo

--- client/src/components/Map/CesiumCameraFlyTo.jsx
+++ client/src/components/Map/CesiumCameraFlyTo.jsx
@@ -1,17 +1,25 @@
 import { Cartesian3 } from 'cesium';
+import { isEmpty } from 'lodash';
 import { useState } from 'react';
 import { CameraFlyTo } from 'resium';
+import { useBridgesState } from '../../context/BridgesContext';
 
 export default function CesiumCameraFlyTo() {
-  const [isCameraReady, setIsCameraReady] = useState(false); // Track if the camera has finished flying
+  const { bridges } = useBridgesState();
+  // Track if the camera has finished flying
+  const [isCameraReady, setIsCameraReady] = useState(false);
+  // start animation of cameraFlyTo
+  const shouldLoadCameraFlyTo = !isCameraReady && !isEmpty(bridges);
 
-  return isCameraReady ? null : (
-    <CameraFlyTo
-      destination={Cartesian3.fromDegrees(-77.194527, 40.003323, 5000000)} // Remove underscore
-      duration={2}
-      onComplete={() => {
-        setIsCameraReady(true);
-      }}
-    />
+  return (
+    shouldLoadCameraFlyTo && (
+      <CameraFlyTo
+        destination={Cartesian3.fromDegrees(-77.194527, 40.003323, 5000000)} // Remove underscore
+        duration={2}
+        onComplete={() => {
+          setIsCameraReady(true);
+        }}
+      />
+    )
   );
 }
